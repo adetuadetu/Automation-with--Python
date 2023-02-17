183135b88cbe57f664d8ab5cf52edc79e47bfc7d
adetuadetu <jposorio95@hotmail.com>
2023-02-17 17:24:31 +0100

    code clean up with nested if else statments

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ name_of_unit = "Hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
 
 
-user_input = input("Hey user, enter a number of days and i will convert it to hours\n")
-user_input_number = int(user_input)
 
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number= int(user_input)
+        if user_input_number > 0:
+             calculated_value = days_to_units(user_input_number)
+             print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
+    else:
+        print("Your input is not a number. Dont ruin my program.")
+
+user_input = input("Hey user, enter a number of days and i will convert it to hours\n")
+validate_and_execute()
