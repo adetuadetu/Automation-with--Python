6e9196b75cf3575c8676338c4c19b8e6c100064a
adetuadetu <jposorio95@hotmail.com>
2023-02-18 19:02:26 +0100

    Implementing list readability

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ def days_to_units(num_of_days):
 
 def validate_and_execute():
     try:
-
-        user_input_number= int(user_input)
+        user_input_number = int(num_of_days_element)
         if user_input_number > 0:
-             calculated_value = days_to_units(user_input_number)
-             print(calculated_value)
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
         elif user_input_number == 0:
             print("you entered a 0, please enter a valid positive number")
         else:
-            print("You entered a negative number, no conversion for you")
-
+            print("You entered a negative number, no conversion for you!")
     except ValueError:
-        print("Your input is not a number. Dont ruin my program.")
+        print("Your input is not a number. Dont ruin my program!")
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hey user, enter a number of days and i will convert it to hours\n")
-    validate_and_execute()
+    user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
+    for num_of_days_element in user_input.split(","):
+        validate_and_execute()
