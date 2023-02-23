1273f6c4a038740129ad2c8ade6f1ff39fb3b1a5
adetuadetu <jposorio95@hotmail.com>
2023-02-23 16:40:07 +0100

    modules

--- lists.py
+++
@@ -1,6 +0,0 @@
-my_list = ["January", "Febuary", "March"]
-print(my_list[2])
-my_list.append("April")
-print(my_list[3])
-my_list.remove("January")
-print(my_list)
--- set.py
+++
@@ -1,8 +0,0 @@
-my_set = {"January", "Febuary", "March"}
-for element in my_set:
-    print(element)
-
-my_set.add("April")
-print(my_set)
-my_set.remove("January")
-print(my_set)
