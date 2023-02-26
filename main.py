adad73a332312bc3eba9fff63f4f5b96d882da75
adetuadetu <jposorio95@hotmail.com>
2023-02-26 21:18:29 +0100

    selecting cells within excel sheet

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ products_per_supplier = {}
 print(product_list.max_row)
 
 
-for product_row in product_list.max_row
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4)
+
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier[supplier_name]
+        products_per_supplier[supplier_name] = current_num_products + 1
