5140d9a7353bae1a2303b7ec35dc04dc6285f067
adetuadetu <jposorio95@hotmail.com>
2023-02-27 09:15:15 +0100

    class's and methods finished

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from user import User
+from post import Post
 
 app_user_one = User("hello@.com", "sarah smith", "pwd", "DevOps Engineer")
 app_user_one.get_user_info()
 
 app_user_two = User("newuser@.com", "james bond", "secret", "Agent")
 app_user_two.get_user_info()
+
+new_post = Post("on a secret mission today", app_user_two.name)
+new_post.get_post_info()
